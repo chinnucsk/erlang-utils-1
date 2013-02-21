24dcef4e33171a67166c999034a6bfab6cf36b5f
David.Gao <david@747.cn>
2013-02-21 19:51:18 +0800

    add sql_tools

---
+++ sql_tools.erl
@@ -0,0 +1,96 @@
+-module(sql_tools).
+-compile(export_all).
+%% Escape character that will confuse an SQL engine
+%% Percent and underscore only need to be escaped for pattern matching like
+%% statement
+escape_like(S) when is_list(S) ->
+    [escape_like(C) || C <- S];
+escape_like($%) -> "\\%";
+escape_like($_) -> "\\_";
+escape_like(C)  -> escape(C).
+
+%% Escape character that will confuse an SQL engine
+escape(S) when is_list(S) ->
+	[escape(C) || C <- S];
+%% Characters to escape
+escape($\0) -> "\\0";
+escape($\n) -> "\\n";
+escape($\t) -> "\\t";
+escape($\b) -> "\\b";
+escape($\r) -> "\\r";
+escape($')  -> "\\'";
+escape($")  -> "\\\"";
+escape($\\) -> "\\\\";
+escape(C)   -> C.
+
+encode(Val) ->
+    encode(Val, false).
+encode(Val, false) when Val == undefined; Val == null ->
+    "NULL";
+encode(Val, true) when Val == undefined; Val == null ->
+    <<"NULL">>;
+encode(Val, false) when is_binary(Val) ->
+    binary_to_list(quote(Val));
+encode(Val, true) when is_binary(Val) ->
+    quote(Val);
+encode(Val, true) ->
+    list_to_binary(encode(Val,false));
+encode(Val, false) when is_atom(Val) ->
+    quote(atom_to_list(Val));
+encode(Val, false) when is_list(Val) ->
+    quote(Val);
+encode(Val, false) when is_integer(Val) ->
+    integer_to_list(Val);
+encode(Val, false) when is_float(Val) ->
+    [Res] = io_lib:format("~w", [Val]),
+    Res;
+encode({datetime, Val}, AsBinary) ->
+    encode(Val, AsBinary);
+encode({{Year, Month, Day}, {Hour, Minute, Second}}, false) ->
+    Res = two_digits([Year, Month, Day, Hour, Minute, Second]),
+    lists:flatten(Res);
+encode({TimeType, Val}, AsBinary)
+  when TimeType == 'date';
+       TimeType == 'time' ->
+    encode(Val, AsBinary);
+encode({Time1, Time2, Time3}, false) ->
+    Res = two_digits([Time1, Time2, Time3]),
+    lists:flatten(Res);
+encode(Val, _AsBinary) ->
+    {error, {unrecognized_value, Val}}.
+
+two_digits(Nums) when is_list(Nums) ->
+    [two_digits(Num) || Num <- Nums];
+two_digits(Num) ->
+    [Str] = io_lib:format("~b", [Num]),
+    case length(Str) of
+	1 -> [$0 | Str];
+	_ -> Str
+    end.
+
+%%  Quote a string or binary value so that it can be included safely in a
+%%  MySQL query.
+
+quote(String) when is_list(String) ->
+    [39 | lists:reverse([39 | quote(String, [])])];	%% 39 is $'
+quote(Bin) when is_binary(Bin) ->
+    list_to_binary(quote(binary_to_list(Bin))).
+
+quote([], Acc) ->
+    Acc;
+quote([0 | Rest], Acc) ->
+    quote(Rest, [$0, $\\ | Acc]);
+quote([10 | Rest], Acc) ->
+    quote(Rest, [$n, $\\ | Acc]);
+quote([13 | Rest], Acc) ->
+    quote(Rest, [$r, $\\ | Acc]);
+quote([$\\ | Rest], Acc) ->
+    quote(Rest, [$\\ , $\\ | Acc]);
+quote([39 | Rest], Acc) ->		    %% 39 is $'
+    quote(Rest, [39, $\\ | Acc]);	%% 39 is $'
+quote([34 | Rest], Acc) ->		    %% 34 is $"
+    quote(Rest, [34, $\\ | Acc]);	%% 34 is $"
+quote([26 | Rest], Acc) ->
+    quote(Rest, [$Z, $\\ | Acc]);
+quote([C | Rest], Acc) ->
+    quote(Rest, [C | Acc]).
