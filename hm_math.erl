30e7fef9cb59a2a00b20352529629a0be03ce002
David.Gao <david.alpha.fox@gmail.com>
2013-05-03 22:19:31 +0800

    make to_hex tail recursive

--- hm_math.erl
+++ hm_math.erl
@@ -6,32 +6,38 @@ int32(Num) ->
     N1 = Num band 16#FFFFFFFF, 
     case N1 =< 16#7FFFFFFF of
         true ->
-	    N1; 
+			N1; 
         false -> 
-	    N1 - 16#FFFFFFFF - 1 
+			N1 - 16#FFFFFFFF - 1 
     end.
 
 int64(Num) ->
     N1 = Num band 16#FFFFFFFFFFFFFFFF,
     case N1 =< 16#7FFFFFFFFFFFFFFF of
-	true ->
-	    N1;
-	false ->
-	    N1 - 16#FFFFFFFFFFFFFFFF -1
+		true ->
+			N1;
+		false ->
+			N1 - 16#FFFFFFFFFFFFFFFF -1
     end.
 
 floor(X) ->
     T = erlang:trunc(X),
     case (X - T) of
-        Neg when Neg < 0 -> T - 1;
-        Pos when Pos > 0 -> T;
-        _ -> T
+        Neg when Neg < 0 ->
+			T - 1;
+        Pos when Pos > 0 -> 
+			T;
+        _ -> 
+			T
     end.
 
 ceil(X) ->
     T = erlang:trunc(X),
     case (X - T) of
-        Neg when Neg < 0 -> T;
-        Pos when Pos > 0 -> T + 1;
-        _ -> T
+        Neg when Neg < 0 -> 
+			T;
+        Pos when Pos > 0 -> 
+			T + 1;
+        _ -> 
+			T
     end.    
--- hm_string.erl
+++ hm_string.erl
@@ -1,14 +1,23 @@
 -module(hm_string).
--export([to_hex/1]).
+-export([to_hex/1,to_ascii/1]).
 
 to_hex([]) ->
     [];
-to_hex(Bin) when is_binary(Bin) -> 
+to_hex(Bin) when erlang:is_binary(Bin) -> 
     to_hex(erlang:binary_to_list(Bin));
-to_hex([H|T]) ->
-    [to_digit(H div 16), to_digit(H rem 16) | to_hex(T)].
+to_hex(List) when erlang:is_list(List) ->
+	to_hex(List,[]).
 
-to_digit(N) when N < 10 ->
+to_hex([],Acc)->
+	lists:reverse(Acc);
+to_hex([H|T],Acc)->
+	Low = to_ascii(H rem 16),
+	High = to_ascii(H div 16),
+	Acc1 = [High | Acc],
+	Acc2 = [Low | Acc1],
+	to_hex(T,Acc2).
+
+to_ascii(N) when N < 10 ->
     $0 + N;
-to_digit(N) ->
+to_ascii(N) ->
     $a + N-10.
