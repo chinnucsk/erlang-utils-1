c56f917f9edb76e56c1d775e314db2073069910f
David.Gao <david.alpha.fox@gmail.com>
2013-05-03 23:07:31 +0800

    add number_to_bool

--- hm_converter.erl
+++ hm_converter.erl
@@ -1,46 +1,51 @@
 -module(hm_converter).
--export([binary_to_list/1,list_to_binary/1]).
--export([bool_to_number/1,term_to_iolist/1]).
+-export([binary_to_list/1,list_to_binary/1,term_to_iolist/1]).
+-export([bool_to_number/1,number_to_bool/1]).
 
 binary_to_list(Bin) when is_binary(Bin) ->
     case unicode:characters_to_binary(Bin,utf8,utf8) of
-      Bin -> 
-        unicode:characters_to_list(Bin);
-      _ ->
-        erlang:binary_to_list(Bin)
+		Bin -> 
+			unicode:characters_to_list(Bin);
+		_ ->
+			erlang:binary_to_list(Bin)
     end.
 
 list_to_binary(L) when is_binary(L) ->
-  L;
+	L;
 
 list_to_binary(L) when is_list(L) ->
   case unicode:characters_to_binary(L) of
       {error,_,_} -> 
-        erlang:list_to_binary(L);
+		  erlang:list_to_binary(L);
       B ->
-         case unicode:characters_to_list(B,utf8) of
-            L -> 
-              B;
-            _ -> 
-              erlang:list_to_binary(L)
-      end
+		  case unicode:characters_to_list(B,utf8) of
+			  L -> 
+				  B;
+			  _ -> 
+				  erlang:list_to_binary(L)
+		  end
   end.
+
+term_to_iolist(Item) when is_binary(Item)->
+	Item;
+term_to_iolist(Item) when is_atom(Item)->
+	erlang:atom_to_binary(Item,utf8);
+term_to_iolist(Item) when is_list(Item)->
+	hm_converter:list_to_binary(Item);
+term_to_iolist(Item) when is_integer(Item)->
+	List = erlang:integer_to_list(Item),
+	erlang:list_to_binary(List);
+term_to_iolist(Item)->
+	erlang:term_to_binary(Item).
 
 bool_to_number(true)->
     1;
 bool_to_number(false) ->
     0;
-bool_to_number(undefined) ->
+bool_to_number(_) ->
     0.
 
-term_to_iolist(Item) when is_binary(Item)->
-  Item;
-term_to_iolist(Item) when is_atom(Item)->
-  erlang:atom_to_binary(Item,utf8);
-term_to_iolist(Item) when is_list(Item)->
-  hm_converter:list_to_binary(Item);
-term_to_iolist(Item) when is_integer(Item)->
-  List = erlang:integer_to_list(Item),
-  erlang:list_to_binary(List);
-term_to_iolist(Item)->
-  erlang:term_to_binary(Item).
+number_to_bool(Num) when Num >= 1->
+	true;
+number_to_bool(_) ->
+	false.
